3526bb7faf50801333e5735e3fba44790cf9be1d
leonardooliveirati <78557233+leonardooliveirati@users.noreply.github.com>
2024-02-21 18:07:45 -0300

    Ajustes indentação scripts

--- ScriptClientesAtraso.sql
+++ ScriptClientesAtraso.sql
@@ -1,7 +1,11 @@
 use FinanceiraClientes
+go
 
-SELECT TOP 4 c.Nome
+SELECT TOP 4 
+	c.Nome
 FROM Clientes c
-INNER JOIN Financiamentos f ON c.ClienteID = f.ClienteID
-INNER JOIN Parcelas p ON f.FinanciamentoID = p.FinanciamentoID
-WHERE p.DataVencimento > GETDATE() AND p.DataPagamento IS NULL;
+	INNER JOIN Financiamentos f ON c.ClienteID = f.ClienteID
+	INNER JOIN Parcelas p ON f.FinanciamentoID = p.FinanciamentoID
+WHERE 
+	p.DataVencimento > GETDATE() 
+AND p.DataPagamento IS NULL;
